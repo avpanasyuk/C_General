8a146c4b67149a2958b6d488ec86aeb05536e94a
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2013-11-30 15:02:50 -0500

    Implemented universal avp::vprintf and avp::printf functions

--- General.h
+++ General.h
@@ -10,6 +10,8 @@
 #define GENERAL_H_
 
 #include <stdint.h>
+#include <stdarg.h>
+#include <stddef.h>
 
 #define N_ELEMENTS(array) (sizeof(array)/sizeof(array[0]))
 
@@ -18,8 +20,12 @@ namespace avp {
   template<typename T> inline constexpr T min(T const& a, T const& b) { return a<b?a:b; }
   template<typename T> inline constexpr T Abs(T const& a) { return a<0?-a:a; }
   template<typename T> inline constexpr T round(T const& num, T const& denom) { return (num + num + denom)/denom/2; }
-  template<typename Tin, typename Tout> inline constexpr Tout sqr(Tin const& a) { return a*a; } 
-} // avp
+  template<typename Tin, typename Tout> inline constexpr Tout sqr(Tin const& a) { return a*a; }
+
+  // following functions use  function of type to do formatted output bool write(void *Ptr, uint16_t Size);
+  extern bool vprintf(bool (*pwrite)(const void *Ptr, size_t Size),char const *format, va_list ap);
+  extern bool printf(bool (*pwrite)(const void *Ptr, size_t Size), char const *format, ...);
+}// avp
 
 #define LOG10(x) ((x)>999?3:(x)>99?2:(x)>9?1:0)
 #define LOG2(x) ((x)>32767?15:(x)>16384?14:(x)>8191?13:(x)>4095?12:(x)>2047?11: \
@@ -32,5 +38,6 @@ inline uint16_t Word(const uint8_t *Params) {
 // following are operators which can be universaly derived from others
 template<typename T> T &operator++(T &v) { return v += 1; }
 template<typename T> T operator++(T &v, int) { T old(v); v += 1; return old; }
-  
+
+
 #endif /* GENERAL_H_ */
--- common_cpp.cpp
+++ common_cpp.cpp
@@ -6,8 +6,23 @@
  */
 // Vector.h
 #include <stdlib.h>
+#include <stdio.h>
 
 namespace avp {
   void (*bad_pointer_func)() = abort;
   void (*bad_index_func)() = abort;
+  bool vprintf(bool (*pwrite)(const void *Ptr, size_t Size),char const *format, va_list ap) {
+    size_t Size = vsnprintf(NULL,0,format,ap);
+    if(Size < 0) return false;
+    char Buffer[Size+1];
+    vsprintf(Buffer,format,ap);
+    return (*pwrite)((void *)Buffer,Size);
+  } // vprintf
+  bool printf(bool (*pwrite)(const void *Ptr, size_t Size), char const *format, ...) {
+    va_list ap;
+    va_start(ap,format);
+    bool Out =  vprintf(pwrite,format,ap);
+    va_end(ap);
+    return Out;
+  } // printf
 }
