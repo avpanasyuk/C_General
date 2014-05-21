3c114d7a04c9a983766a9e12a326b4a209e6a11d
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2014-05-21 11:30:36 -0400

    Using contexpr

--- General.h
+++ General.h
@@ -19,15 +19,17 @@ namespace avp {
   template<typename T> inline constexpr T max(T const& a, T const& b) { return a>b?a:b; }
   template<typename T> inline constexpr T min(T const& a, T const& b) { return a<b?a:b; }
   template<typename T> inline constexpr T Abs(T const& a) { return a<0?-a:a; }
-  template<typename T> inline constexpr T round(T const& num, T const& denom) { return (num + num + denom)/denom/2; }
-  template<typename Tin, typename Tout> inline constexpr Tout sqr(Tin const& a) { return Tout(a)*Tout(a); }
+  template<typename T> inline constexpr T RoundRatio(T const& num, T const& denom) 
+		{ return (num + num + denom)/denom/2; }
+  template<typename Tin, typename Tout> inline constexpr Tout sqr(Tin const& a) 
+		{ return Tout(a)*Tout(a); }
 
   // following functions use  function of type to do formatted output bool write(void *Ptr, uint16_t Size);
   // NOTE both functions do not write string-ending 0 !!!!!
   extern bool vprintf(bool (*pwrite)(const void *Ptr, size_t Size),char const *format, va_list ap);
   extern bool printf(bool (*pwrite)(const void *Ptr, size_t Size), char const *format, ...);
-  static inline uint16_t Word(const uint8_t *Params) {
-    return (uint16_t(Params[1]) << 8)+Params[0];
+  static inline constexpr uint16_t Word(const uint8_t *Bytes) {
+    return (uint16_t(Bytes[1]) << 8)+Bytes[0];
   } // Word
   template<class ElType, class SzType> ElType checkXOR(const ElType *p, SzType size) {
     ElType XORvalue = 0;
@@ -40,12 +42,14 @@ namespace avp {
     while(size--) out += *(p++);
     return out;
   } // checksum
+	// template<typename type> uint8_t log2(type x) { uint8_t out=0; while(x>>=1) out++; return out; }
+	template<typename type> constexpr int8_t log2(type x) { return x?log2<type>(x>>1)+1:-1; }
 }// avp
 
 #define LOG10(x) ((x)>999?3:(x)>99?2:(x)>9?1:0)
-#define LOG2(x) ((x)>32767?15:(x)>16384?14:(x)>8191?13:(x)>4095?12:(x)>2047?11: \
-(x)>1023?10:(x)>511?9:(x)>255?8:(x)>127?7:(x)>63?6:(x)>31?5:(x)>15?4:(x)>7?3:(x)>3?2:(x)>1?1:0)
-
+// #define LOG2(x) ((x)>32767?15:(x)>16384?14:(x)>8191?13:(x)>4095?12:(x)>2047?11: \
+// (x)>1023?10:(x)>511?9:(x)>255?8:(x)>127?7:(x)>63?6:(x)>31?5:(x)>15?4:(x)>7?3:(x)>3?2:(x)>1?1:0)
+#define LOG2(x) avp::log2(x)
 
 // following are operators which can be universaly derived from others
 template<typename T> T &operator++(T &v) { return v += 1; }
