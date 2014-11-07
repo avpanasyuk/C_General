8495173baee4a365918a59248dfe1d7a7f8b67da
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2014-11-07 10:18:34 -0500

    Fixes for NON-DEBUG mode

--- Error.h
+++ Error.h
@@ -26,28 +26,21 @@ namespace Fail {
 #define AVP_ERROR(format, ...) do{ \
  avp::printf<avp::vprintf<avp::error_output> >("Error in file %s, line %d: " format, \
              __FILE__, __LINE__, ##__VA_ARGS__); avp::major_fail(); }while(0)
-// #else
-// #define AVP_ERROR(format,...) major_fail();
-#endif
-
-// "exp" should return 0 if success and error code if not
-#ifdef DEBUG
-
 // POLICY: AVP_ASSERT is used just before a deadly operation, like assigning zero
 // pointer, and not preliminary
-
 #define AVP_ASSERT_(exp,err_form,...) do{ if((avp::AssertError = !(exp))) \
 { AVP_ERROR("Expression (%s) is false in %s on line %d" err_form "!\n", \
             #exp, __FILE__, __LINE__,##__VA_ARGS__); }}while(0)
-#define AVP_ASSERT(exp) AVP_ASSERT_(exp,"")
-#define ASSERT_BEING_0(exp) do{ if((avp::AssertError = (exp))) \
+#define ASSERT_BEING_0(exp) do{ if(avp::AssertError = (exp)) \
   { AVP_ERROR("Expression (%s) equals %d != 0 in %s on line %d!\n", \
             #exp, avp::AssertError, __FILE__, __LINE__); }}while(0)
-#define AVP_ASSERT_ERRNUM(exp, ERRNUM) AVP_ASSERT_(exp,", error #%d", ERRNUM)
 #else
-#define AVP_ASSERT(exp,...) { if((avp::AssertError = !(exp))) avp::major_fail(##__VA_ARGS__); }
-#define AVP_ASSERT_ERRNUM(exp,...) AVP_ASSERT(exp,##_VA_ARGS)
-#define ASSERT_BEING_0(exp,...) { if((avp::AssertError = (exp))) avp::major_fail(##__VA_ARGS__); }
+#define AVP_ERROR(format,...) avp::major_fail()
+#define AVP_ASSERT_(exp,...) do{ (exp); }while(0)
+#define ASSERT_BEING_0(exp,...) AVP_ASSERT(exp)
 #endif
+
+#define AVP_ASSERT(exp) AVP_ASSERT_(exp,"")
+#define AVP_ASSERT_ERRNUM(exp, ERRNUM) AVP_ASSERT_(exp,", error #%d", ERRNUM)
 
 #endif /* ERROR_H_INCLUDED */
