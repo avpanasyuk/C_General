65f915f8478a54c24ac21b52a9a5489732da7a4a
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2015-10-05 11:00:25 -0400

    Changes of bit functions to get them more intuitive (hmm)

--- BitBang.h
+++ BitBang.h
@@ -8,9 +8,13 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include "Math.h"
 
 namespace avp {
   // bit-banging functions
+  template<typename type>
+  inline constexpr uint8_t num_bits_in() { return avp::log2(uint32_t(type(-1))+1); }
+
   static inline constexpr uint16_t Word(const uint8_t *Bytes) {
     return (uint16_t(Bytes[1]) << 8)+Bytes[0];
   } // Word
@@ -21,8 +25,9 @@ namespace avp {
   } // checksum
 
   // ***** BIT HANDLING FUNCTIONS
-  template<typename type> inline constexpr type make_mask(uint8_t lowest_bit, uint8_t numbits) {
-    return ((type(1) << numbits) - 1) << lowest_bit;
+  template<typename type>
+  inline constexpr type make_mask(uint8_t numbits = num_bits_in<type>(), uint8_t lowest_bit = 0) {
+    return ((type(1U) << numbits) - 1) << lowest_bit;
   }
   // SETTING BITS
   template<typename type> inline void set_high(type &var, uint8_t bitI) { var |= 1 << bitI; }
@@ -31,22 +36,34 @@ namespace avp {
   template<typename type> inline void setbit(type &var, uint8_t bitI, bool value) {
     value?set_high(var,bitI):set_low(var,bitI);
   } // setbit
-  template<typename type, typename type1> inline void setbits(type &var, uint8_t lowest_bit, uint8_t numbits, type1 value) {
-    var = (var & ~make_mask<type>(lowest_bit,numbits)) | (value << lowest_bit);
+  template<typename type, typename type1>
+  inline void setbits(type &var, type value, type mask = make_mask<type>()) {
+    var = (var & ~mask) | (value & mask);
+  }
+  template<typename type, typename type1 = type>
+  inline void setbits(type &var, type1 value, uint8_t numbits = num_bits_in<type>(), uint8_t lowest_bit = 0) {
+    setbits(var,value << lowest_bit,make_mask<type>(numbits,lowest_bit));
   }
   // GETTING BITS
   //! single bit
   template<typename type> inline constexpr bool getbit(type const &var,uint8_t bitI) {
     return (var >> bitI) & 1;
   }
-  //! range of bits
-  template<typename type> inline constexpr type bits(type x, uint8_t first_bit, uint8_t last_bit) {
-    return (x >> first_bit) & ((type(1U) << (last_bit - first_bit + 1)) - 1);
+  //! get value from a range of bits
+  template<typename type> inline constexpr type bits(type x, uint8_t numbits, uint8_t lowest_bit = 0) {
+    return (x >> lowest_bit) & make_mask<type>(numbits, lowest_bit);
   } // bits
-  //! when last_bit is the last bit
-  template<typename type> inline constexpr type bits(type x, uint8_t first_bit) {
-    return x >> first_bit;
-  } // bits
+
+  // converting a pattern in memory to type
+  template<typename T, uint8_t NumChars = sizeof(T)>
+  static inline constexpr T Bytes2type(const uint8_t bytes[NumChars]) {
+    return T(bytes[0]) + (NumChars == 1?0:(Bytes2type<T,NumChars-1>(bytes+1) << 8));
+  } // Chars2type
+
+  template<typename T, uint8_t NumChars = sizeof(T)>
+  static inline constexpr T Chars2type(const char str[NumChars]) {
+    return Bytes2type<T,NumChars>((const uint8_t *)str);
+  } // Chars2type
 } // namespace avp
 
 
