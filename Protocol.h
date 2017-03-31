fb94f74412d48f21ea4a4e6bc517b63b1c82f6d6
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2017-03-30 21:16:49 -0400

    Minor

--- Protocol.h
+++ Protocol.h
@@ -34,10 +34,10 @@
 
   @section StartHandshake Initial Handshake.
   There may be several physical and virtual COM ports in the system, and we got to determine which
-  one device is connected to. This class supports automatic port finding. It sends out avp::Protocol::BeaconStr
+  one device is connected to. This class supports automatic port finding. It sends out '2XjA'
   every time avp::Protocol::SendBeacon is called (which should be done continuously before connection). The program on the
   opposite end of connection should
-    -# (optional) checks every port until it finds one continuously transmitting avp::Protocol::BeaconStr.
+    -# (optional) checks every port until it finds one continuously transmitting '2XjA'.
     -# leave the transmitting port open (or reopen it)
     -# sends NOOP command (which a single 0 byte) to the port
     -# immediately start monitoring incoming stream on the presence of four 0 byte sequence
@@ -46,6 +46,14 @@
       sending out four 0 bytes - one for status, two for size and the last one as checksum. It immediately
     -# when the communicating program receives four 0 byte sequence it should continue communication
       using \ref ProtocolDescription "Protocol Description".
+
+  @section Resynchronization Resynchronization
+  If a byte or two got lost communication may hang, because e.g. the FW may wait for missing parameter bytes, so
+  it would not process the command and send out return.
+  To resync the protocol communicating program may be sending NOOPs (single 0 byte) one by one.
+  Eventually FW will receive all byte it was waiting for, return "bad checksum" error status and  and starts responding
+  with four 0 byte on every NOOP received. When it happens the protocol is resynchronized.
+
   */
 
 #ifndef COMMAND_PROTOCOL_HPP_INCLUDED
