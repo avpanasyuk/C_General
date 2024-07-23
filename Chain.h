871e5fcfbc8b21e522db7205fcd314a3bd2463c2
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2024-07-23 13:55:00 -0400

    Improving class "Chain"

--- Chain.h
+++ Chain.h
@@ -3,14 +3,20 @@
 #include <../C_General/Error.h>
 
 namespace avp {
+  /**
+    This class allows to link, e.g. instances of static template classes by including into each a static field inherited from "Link"
+  */
   struct Chain {
+    /**
+    this is pure virtual which does not include key
+    */
     class Link {
       Link *pPrev, *pNext;
      public:
-       class Comparator {
-         public:
-         virtual bool IsIt(const Link *p) const = 0;
-       }; // class Comparator
+      class Comparator {
+       public:
+        virtual bool IsIt(const Link *pLink) const = 0;
+      }; // class Comparator
       Link(): pPrev(nullptr), pNext(nullptr) {}
       Link(Chain *pChain) {
         pChain->Append(this);
@@ -22,10 +28,10 @@ namespace avp {
       /**
       */
       friend class Chain;
-    };
+    }; // class Link
 
     Chain():pEnd(&Start) {}
-    ~Chain() {
+    virtual ~Chain() {
       while(pEnd != &Start) delete pEnd;
     } // destructor
 
@@ -76,4 +82,35 @@ namespace avp {
     Link Start, *pEnd; //!< Link Start is fudge Link to avoid checking for nullptr all the time
     //!< Link Start has pPrev == nullptr, that's how we detect it
   }; // class Chain
+
+  struct ChainByPointer: public Chain {
+    class Link: public Chain::Link {
+      const void *p; // key
+     public:
+      class Comparator: public Chain::Link::Comparator {
+        const void *p; // key
+       public:
+        Comparator(const void *p_): p(p_) {}
+        virtual bool IsIt(const Chain::Link *pLink) const {
+          return ((const Link *)pLink)->p == p;
+        };
+      }; // class Comparator
+      Link(const void *p_): p(p_) {}
+      Link(const void *p_, Chain *pChain): Chain::Link(pChain), p(p_) {}
+    }; // class LinkPointer
+
+    /**
+    @param fComp: find first Link for which "fComp" returns true
+    */
+    const Link *FindFirst(const void *p) const {
+      return (const Link *)Chain::FindFirst(Link::Comparator(p));
+    } // FindFirst
+
+    /**
+    @param fComp: find last Link for which "fComp" returns true
+    */
+    const Link *FindLast(const void *p) const {
+      return (const Link *)Chain::FindLast(Link::Comparator(p));
+    } // FindLast
+  }; // class ChainByPointer
 } // namespace avp
