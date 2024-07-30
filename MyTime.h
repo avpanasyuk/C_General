5d44fa2713e6039fcc1ae6063ed7639c22b4f3f3
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2024-07-30 14:00:20 -0400

    TimeInterval added

--- MyTime.h
+++ MyTime.h
@@ -6,11 +6,12 @@
 
 #pragma once
 
-  /// @cond
-    // millis() and micros() should defined beforehand and return microseconds and milliseconds
+/// @cond
+// millis() and micros() should defined beforehand and return microseconds and milliseconds
 
 #include <stdint.h>
 #include <limits.h>
+#include <type_traits>
 /// @endcond
 #include "General.h"
 
@@ -20,37 +21,62 @@ extern "C" uint32_t micros();
 namespace avp {
   /// !!!! Periods and Delays should not be longer than half of value which fits into typename!
   typedef decltype(millis()) Time_t;
-
+  static_assert(std::is_unsigned<Time_t>::value, "Time_t should be unsigned!");
 
   /**
    * Using: avp::Timeout<> Time_t; while(something) { if(Time_t) { error('Timed out!); break; }}
-   * @tparam Time_t should be unsigned!
+   * @tparam TickFunction, millis() by default
    */
   template<Time_t (*TickFunction)() = millis>
   class TimeOut {
     const Time_t When;
-  public:
+   public:
     TimeOut(Time_t Timeout) : When(TickFunction() + Timeout) { }
-    operator bool() const { return unsigned_is_smaller(When, TickFunction()); } // true when expired
+    operator bool() const {
+      return unsigned_is_smaller(When, TickFunction());  // true when expired
+    }
   }; // TimeOut
+
+  template<Time_t (*TickFunction)() = millis>
+  class TimeInterval {
+    bool IsSet;
+    Time_t End;
+   public:
+    TimeInterval(): IsSet(false) {}
+
+    void Start(Time_t Interval) {
+      End = TickFunction() + Interval;
+      IsSet = true;
+    } // Start
+
+    bool Expired() {
+      if(IsSet && unsigned_is_smaller(End, TickFunction())) {
+        IsSet = false;
+        return true;   // true when expired
+      }
+      return false;
+    } // Expired
+  }; // TimeInterval
 
   //! @tparam Time_t should be unsigned!
   template<Time_t (*TickFunction)() = millis>
   class TimePeriod {
     Time_t NextTime, Period;
-  public:
-    void Reset() { NextTime = TickFunction() + Period; }
+   public:
+    void Reset() {
+      NextTime = TickFunction() + Period;
+    }
     /**
     * @param Timeout in whatever units TickFunction works
     */
     TimePeriod(Time_t Timeout) :Period(Timeout) {
-      static_assert(Time_t(0) < Time_t(-1), "Time_t should be unsigned!");
-      // AVP_ASSERT(Timeout < std::numeric_limits<Time_t>::max()/2);
       Reset();
     }
     /// just checks whether TimePeriod had passed, does not do reset
     /// if there is no Reset for too long the counter may wrap over
-    bool JustCheck() const { return unsigned_is_smaller(NextTime, TickFunction()); }
+    bool JustCheck() const {
+      return unsigned_is_smaller(NextTime, TickFunction());
+    }
 
     /// if TimePeriod had passed does reset to start a new TimePeriod
     bool Expired() {
@@ -59,7 +85,9 @@ namespace avp {
       return Out;
     } // Passed
 
-    operator Time_t&() { return Period; }
+    operator Time_t&() {
+      return Period;
+    }
 
     /// Pause with an internal loop
     static void Pause(Time_t Delay, void (*LoopFunc)()) {
@@ -67,22 +95,24 @@ namespace avp {
       while(!Timer.Expired()) (*LoopFunc)();
     } // Pause
 
-    static void Pause(Time_t Delay) { Pause(Delay, []() { }); }
+    static void Pause(Time_t Delay) {
+      Pause(Delay, []() { });
+    }
   }; // TimePeriod
 
   /**
    *@brief we Run called in loop executes function with a given period
    * Example:
-void loop() {
+  void loop() {
   avp::Periodically<SendData>::Run(1000);
-}
+  }
    *
    * @tparam (*Func)()
    */
   template<void (*Func)(), Time_t(*TickFunction)() = millis>
   class Periodically {
     static Time_t NextTime;
-  public:
+   public:
     static void Run(Time_t Period) {
       Time_t Now = TickFunction();
       if(unsigned_is_smaller(NextTime, Now)) {
@@ -99,10 +129,15 @@ void loop() {
   template<Time_t(*TickFunction)(), void (*Func)(), Time_t Period>
   class RunPeriodically {
     static TimePeriod<TickFunction> TP;
-  public:
-    static void cycle() { if(TP.Expired()) (*Func)(); } // cycle
-    static void Reset() { TP.Reset(); }
+   public:
+    static void cycle() {
+      if(TP.Expired()) (*Func)();  // cycle
+    }
+    static void Reset() {
+      TP.Reset();
+    }
   }; // RunPeriodically
+
 
   template<Time_t(*TickFunction)(), void (*Func)(), Time_t Period>
   TimePeriod<TickFunction> RunPeriodically<TickFunction, Func, Period>::TP(Period);
