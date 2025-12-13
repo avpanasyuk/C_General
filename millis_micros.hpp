1eece829ac975b334e923325dd27fcb2bd348e5d
avpanasyuk <alexander.panasyuk@gmail.com>
2025-12-13 17:23:41 -0500

    Added ESP timers

--- millis_micros.hpp
+++ millis_micros.hpp
@@ -2,6 +2,29 @@
 
 #include <stdint.h>
 
+#ifdef ESP32
+#include <esp32-hal.h>
+//#include "esp_timer.h"
+
+// uint32_t micros() {
+//     return (uint32_t) esp_timer_get_time();
+// }
+
+// uint32_t millis() {
+//     return (uint32_t) (esp_timer_get_time() / 1000ULL);
+// }
+#endif
+
+#ifdef ESP8266
+uint32_t micros() {
+    return system_get_time();
+}
+
+uint32_t millis() {
+    return system_get_time() / 1000;
+}
+#endif
+
 #ifndef NO_STL
 #include <chrono>
 
